b8aa2e16e4d8ad0a64c40572c13b347b43e62ad6
Michael Torres <mic.ric.tor@gmail.com>
2025-09-26 17:48:04 -0500

    Add demo

---
+++ demo_shellai.py
@@ -0,0 +1,15 @@
+from shellai import ShellAI
+
+m = ShellAI()
+m.load_model()
+
+
+for prompt in [
+    "what are the files bigger than 1mb in the current directory",
+    "find files modified in the last 24 hours",
+    "compress all .txt files into an archive",
+    "show me the total disk usage of my home directory",
+    "search for \"EGG\" in all ELF files in /usr/bin",
+]: 
+    print(f"Prompt: {prompt}")
+    print(f"Generated command: {m.generate_command(prompt)}")
